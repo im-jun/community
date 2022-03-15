4849cd4d91962424f689ab2782404735ac3733df
im-jun <2277651605@qq.com>
2022-03-15 22:15:57 +0800

    重构代码，加入拦截器来判断用户是否登录

--- src/main/java/life/nujiew/community/controller/IndexController.java
+++ src/main/java/life/nujiew/community/controller/IndexController.java
@@ -1,11 +1,6 @@
 package life.nujiew.community.controller;
 
 import life.nujiew.community.dto.PaginationDTO;
-import life.nujiew.community.dto.QuestionDTO;
-import life.nujiew.community.mapper.QuestionMapper;
-import life.nujiew.community.mapper.UserMapper;
-import life.nujiew.community.model.Question;
-import life.nujiew.community.model.User;
 import life.nujiew.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -13,43 +8,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
-
 
 @Controller
 public class IndexController {
-    @Autowired
-    private UserMapper userMapper;
-
     @Autowired
     private QuestionService questionService;
 
     // 主页
     @GetMapping("/")
-    public String Index(HttpServletRequest request,
-                        Model model,
+    public String Index(Model model,
                         @RequestParam(name = "page", defaultValue = "1") Integer page,
                         @RequestParam(name = "size", defaultValue = "5") Integer size){
-        // 通过cookie判断登录状态
-        // 访问首页时，循环查看所有cookie，找到对应name的cookie，获取该cookie的value
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    // 通过token查找数据库中是否存在对应记录的用户
-                    User user = userMapper.findByToken(token);
-                    // 有的话将其放入session，前端就能判断是否对user信息进行展示
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
-
         // 获取帖子列表
         PaginationDTO pagination = questionService.list(page,size);
         model.addAttribute("pagination", pagination);
--- src/main/java/life/nujiew/community/controller/PublishController.java
+++ src/main/java/life/nujiew/community/controller/PublishController.java
@@ -1,7 +1,6 @@
 package life.nujiew.community.controller;
 
 import life.nujiew.community.mapper.QuestionMapper;
-import life.nujiew.community.mapper.UserMapper;
 import life.nujiew.community.model.Question;
 import life.nujiew.community.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,16 +10,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PublishController {
     @Autowired
     private QuestionMapper questionMapper;
-
-    @Autowired
-    private UserMapper userMapper;
 
     // 发布页面
     @GetMapping("/publish")
@@ -53,22 +48,7 @@ public class PublishController {
         }
 
         // 获取user，判断是否登录
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    // 通过token查找数据库中是否存在对应记录的用户
-                    user = userMapper.findByToken(token);
-                    // 有的话将其放入session，前端就能判断是否对user信息进行展示
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
+        User user = (User) request.getSession().getAttribute("user");
         if (user == null) {
             model.addAttribute("error", "用户未登录!");
             return "publish";
